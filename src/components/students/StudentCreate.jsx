da786d6b794d46b155a65a5f89f90ad97348a846
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2024-01-01 22:17:01 +0700

    menambahkan logika pada form

--- src/components/students/StudentCreate.jsx
+++ src/components/students/StudentCreate.jsx
@@ -1,9 +1,21 @@
 /** @format */
 
-import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const StudentCreate = () => {
+  const [formValues, setFormValues] = useState({
+    npm: "",
+    nama: "",
+    kelas: "",
+    jurusan: "",
+    nohp: "",
+  });
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
   return (
     <div className='mt-12'>
       <form className='max-w-md p-4 mx-auto bg-white rounded-xl shadow-md '>
@@ -16,35 +28,45 @@ export const StudentCreate = () => {
               NPM
             </label>
             <input
-              type='text'
+              name='npm'
+              value={formValues["npm"]}
+              onChange={onChange}
               className='block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-md'
             />
             <label htmlFor='nama' className='block mb-2 text-sm font-medium'>
               Nama
             </label>
             <input
-              type='text'
+              name='nama'
+              value={formValues["nama"]}
+              onChange={onChange}
               className='block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-md'
             />
             <label htmlFor='kelas' className='block mb-2 text-sm font-medium'>
               Kelas
             </label>
             <input
-              type='text'
+              name='kelas'
+              value={formValues["kelas"]}
+              onChange={onChange}
               className='block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-md'
             />
             <label htmlFor='jurusan' className='block mb-2 text-sm font-medium'>
               Jurusan
             </label>
             <input
-              type='text'
+              name='jurusan'
+              value={formValues["jurusan"]}
+              onChange={onChange}
               className='block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-md'
             />
             <label htmlFor='nohp' className='block mb-2 text-sm font-medium'>
               Nomor Telepon
             </label>
             <input
-              type='text'
+              name='nohp'
+              value={formValues["nohp"]}
+              onChange={onChange}
               className='block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-md'
             />
           </div>
